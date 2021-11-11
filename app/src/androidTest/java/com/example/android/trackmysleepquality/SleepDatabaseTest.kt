cc371c050b923dc8bc24f05f1fa264afc08625fc
Kevin Beaton <kabton14@gmail.com>
2021-11-11 01:25:46 -0500

    Add more tests

--- app/src/androidTest/java/com/example/android/trackmysleepquality/SleepDatabaseTest.kt
+++ app/src/androidTest/java/com/example/android/trackmysleepquality/SleepDatabaseTest.kt
@@ -24,6 +24,7 @@ import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 import com.example.android.trackmysleepquality.database.SleepNight
 import org.junit.Assert.assertEquals
 import org.junit.After
+import org.junit.Assert.assertNull
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -66,6 +67,26 @@ class SleepDatabaseTest {
         sleepDao.insert(night)
         val tonight = sleepDao.getTonight()
         assertEquals(tonight?.sleepQuality, -1)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun update() {
+        var night = SleepNight()
+        sleepDao.insert(night)
+        night.sleepQuality = 10
+        sleepDao.update(night)
+        assertEquals(night.sleepQuality, 10)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun clear() {
+        val night = SleepNight()
+        sleepDao.insert(night)
+        sleepDao.clear()
+        val nights = sleepDao.getTonight()
+        assertNull(nights)
     }
 }
 
