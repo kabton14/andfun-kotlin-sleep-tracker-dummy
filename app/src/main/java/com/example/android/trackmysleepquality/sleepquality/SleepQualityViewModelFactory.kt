ac043189f81b866ed981a01196340764eed2c638
Kevin Beaton <kabton14@gmail.com>
2021-11-15 20:50:46 -0500

    Create SleepQualityViewModelFactory

--- app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModelFactory.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModelFactory.kt
@@ -15,3 +15,21 @@
  */
 
 package com.example.android.trackmysleepquality.sleepquality
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import java.lang.IllegalArgumentException
+import javax.sql.CommonDataSource
+
+class SleepQualityViewModelFactory(
+    private val sleepNightKey: Long,
+    private val dataSource: SleepDatabaseDao) : ViewModelProvider.Factory {
+        @Suppress("unchecked_cast")
+        override fun <T : ViewModel?> create(modeClass: Class<T>): T {
+            if (modeClass.isAssignableFrom(SleepQualityViewModel::class.java)) {
+                return SleepQualityViewModel(sleepNightKey, dataSource) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
